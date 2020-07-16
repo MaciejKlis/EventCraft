1899eb95b3a24c50e9df1beb8bf38014a1f6e1e7
MaciejKlis <maciej.klis@thecoders.pl>
2020-07-16 13:49:54 +0200

    New feat(): Color picker to change bakcground

--- apps/eventcraft-app/src/app/app.component.ts
+++ apps/eventcraft-app/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,7 +10,24 @@ import { Store } from '@ngxs/store';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store){}
-  
-  ngOnInit() {}
+  constructor(private store: Store, private router: Router) { }
+
+  @HostBinding('style.background') background = "#262939"
+  colorPicker: string;
+  isHomePage = true;
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        map(event => event.urlAfterRedirects)
+      )
+      .subscribe(url => {
+        this.isHomePage = url === '/' ? true : false;
+      })
+  }
+
+  changeBackground() {
+    this.background = this.colorPicker;
+  }
 }
